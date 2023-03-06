6863c49f181e12f56f9fc09623aa1662f5ecfd02
Mukul <mukul.dharashivkar99@gmail.com>
2023-03-06 10:05:29 +0530

    2nd Commit

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,9 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-const MealItemForm = (props) => {
+const MealItemForm = React.forwardRef((props, ref) => {
   const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
@@ -44,6 +44,6 @@ const MealItemForm = (props) => {
       {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
-};
+});
 
 export default MealItemForm;
